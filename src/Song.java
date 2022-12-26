c8ee32e2fe8ff96f1e8436f51f7e09884d962c03
Akash Kumar <kr.akash149@gmail.com>
2022-12-26 16:26:04 +0530

    PlayList and Song code

---
+++ src/Song.java
@@ -0,0 +1,40 @@
+public class Song {
+    private String SongName;
+    private String Artist;
+    private String Duration;
+
+    public Song(String SongName, String Artist, String Duration) {
+        this.SongName = SongName;
+        this.Artist = Artist;
+        this.Duration = Duration;
+    }
+    public void showSong(){
+        System.out.println(this.getSongName()+"----->"+this.getArtist()+"----->"+this.getDuration());
+    }
+
+    public String getSongName() {
+        return SongName;
+    }
+
+    public void setSongName(String SongName) {
+        this.SongName = SongName;
+    }
+
+    public String getArtist() {
+        return Artist;
+    }
+
+    public void setArtist(String Artist) {
+        this.Artist = Artist;
+    }
+
+    public String getDuration() {
+        return Duration;
+    }
+
+    public void setDuration(String Duration) {
+        this.Duration = Duration;
+    }
+
+}
+
